60e0307ab078d26b9dc3de921c98a2cba4a7af10
Desipeli <mr.desipeli@gmail.com>
2023-06-02 23:18:35 +0300

    testi

---
+++ nav.js
@@ -0,0 +1,17 @@
+const headnav = document.getElementById("head-nav")
+headnav.innerHTML = `
+<header>
+    <b><a href="index.html">LILITH</a></b>
+</header>
+<nav>
+<div id="nav-links">
+    <a href="esittely.html">Esittely</a>
+    <a href="kuvat.html">Kuvat</a>
+    <a href="#">Videot</a>
+    <a href="#">Media</a>
+</div>
+
+<div id="nav-hamburger">
+    <h1>Valikko</h1>
+</div>
+</nav>`
